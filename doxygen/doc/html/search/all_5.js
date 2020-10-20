GiuliaP/stereo-vision

provided doxygen documentation

doxygen/doc/html/search/all_5.js
@@ -1,0 +1,5 @@
+var searchData=
+[
+  ['horn',['horn',['../classStereoCamera.html#adf155975709fdbf09d3133899d074a02',1,'StereoCamera']]],
+  ['hornrelativeorientations',['hornRelativeOrientations',['../classStereoCamera.html#a36dae897d07544e8faca8c90216173a6',1,'StereoCamera']]]
+];

robotology/stereoVision

doxygen/doc/html/search/all_5.js
@@ -1,0 +1,28 @@
+var searchData=
+[
+  ['getcameramatrix',['getCameraMatrix',['../classCamera.html#af1433656f08f22aa1080d0c6e7a98df6',1,'Camera']]],
+  ['getdisparity',['getDisparity',['../classStereoCamera.html#a74adec3c305b13df4757e105cd72a7db',1,'StereoCamera']]],
+  ['getdisparity16',['getDisparity16',['../classStereoCamera.html#a4a6e408446750224499ed9ecdc51629e',1,'StereoCamera']]],
+  ['getdistcoeffleft',['getDistCoeffLeft',['../classStereoCamera.html#af9e52f941837f0db26d150d0ca4ff3bc',1,'StereoCamera']]],
+  ['getdistcoeffright',['getDistCoeffRight',['../classStereoCamera.html#a589ca8eb4ebfc4377589a77bb15b85b7',1,'StereoCamera']]],
+  ['getdistortedpixel',['getDistortedPixel',['../classStereoCamera.html#af64550bb8a7a665930dee5eea87b7e47',1,'StereoCamera']]],
+  ['getdistvector',['getDistVector',['../classCamera.html#a1cd15bc4fd4f0405c7e047af632b9353',1,'Camera']]],
+  ['getfundamental',['getFundamental',['../classStereoCamera.html#ab02e0869a054fe23d2b56def81a1a947',1,'StereoCamera']]],
+  ['getimleft',['getImLeft',['../classStereoCamera.html#ac0cb76a6994bd7ab915cb02d83cf8f8e',1,'StereoCamera']]],
+  ['getimleftund',['getImLeftUnd',['../classStereoCamera.html#a52ba4f0331f3690ca0f983b10bd0c783',1,'StereoCamera']]],
+  ['getimright',['getImRight',['../classStereoCamera.html#ac7930aa4fa0681246e74c08cf4b1079b',1,'StereoCamera']]],
+  ['getimrightund',['getImRightUnd',['../classStereoCamera.html#a19009c20003a5c7e957a4b3c34913fe9',1,'StereoCamera']]],
+  ['getkleft',['getKleft',['../classStereoCamera.html#a932e0ebd63c3e9a93b33c1846459aadc',1,'StereoCamera']]],
+  ['getkright',['getKright',['../classStereoCamera.html#a6f9e53e949b77cd19e28a4ec92034a3d',1,'StereoCamera']]],
+  ['getlrectified',['getLRectified',['../classStereoCamera.html#ad81c9f7f4f15ef3f1e61d34fd8df9856',1,'StereoCamera']]],
+  ['getmapperl',['getMapperL',['../classStereoCamera.html#ae1c7ef2cc1a3de5d10c6bb39fbb78719',1,'StereoCamera']]],
+  ['getmapperr',['getMapperR',['../classStereoCamera.html#ac3f7763fdb6ed57f96924bb0b32b4ea7',1,'StereoCamera']]],
+  ['getmatchleft',['getMatchLeft',['../classStereoCamera.html#aa50cb648f92d099e1ffbb7bab57c3fc3',1,'StereoCamera']]],
+  ['getmatchright',['getMatchRight',['../classStereoCamera.html#ac8b01bc577cc3de8e7e8bc246d5c50f0',1,'StereoCamera']]],
+  ['getq',['getQ',['../classStereoCamera.html#a9f10e66261a0211e5d786abc3df8a70b',1,'StereoCamera']]],
+  ['getrlrect',['getRLrect',['../classStereoCamera.html#ab3adb747963c617fd2fe1a919b0c59c7',1,'StereoCamera']]],
+  ['getrotation',['getRotation',['../classStereoCamera.html#a08b520f9976fc3213047844fedc02a54',1,'StereoCamera']]],
+  ['getrrectified',['getRRectified',['../classStereoCamera.html#aff19b07a766c01d04ed623a05ef4d67d',1,'StereoCamera']]],
+  ['getrrrect',['getRRrect',['../classStereoCamera.html#a44e111911af89a299b134fd91b4bc7d6',1,'StereoCamera']]],
+  ['gettranslation',['getTranslation',['../classStereoCamera.html#a3ca6d46fc45835bff5fa0a5753ca40c0',1,'StereoCamera']]]
+];